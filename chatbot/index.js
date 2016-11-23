4e8e7b507da718a4130fae902146760c2624cc01
Sashi Kumar Gupta <sgupta_1@cpapi-sandbox-ukapi-m03.ihost.aol.com>
2016-11-23 05:59:55 -0500

    updated the bot to speak

--- chatbot/index.js
+++ chatbot/index.js
@@ -26,6 +26,40 @@ app.get('/webhook/', function (req, res) {
     res.send('Error, wrong token')
 })
 
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+const token = "EAAE8weXTAi0BAL2MZAaG7wV0MEVD8w49bBvO6ql2OLZA1aB1SVeSntJ7vyOl7V5CAchAMGOXHKqIRvPVuYG6OvKiOSDsEZC7vJ7akCZBLXD2u9PjKEcsDPClQKqQdZAZCBCevubxLAdCaAUBD0GDD2qCUfChBhOp3mhvHKyTRJqQZDZD"
+
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
